7c5f3bd52426cb37039ee54cfe62d88cb198e125
hrabid <hrabid@outlook.com>
2026-06-25 23:17:24 +0600

    configured kvantum

--- dot_config/hypr/modules/env.lua
+++ dot_config/hypr/modules/env.lua
@@ -25,6 +25,7 @@ hl.env("QT_AUTO_SCREEN_SCALE_FACTOR", "1")
 hl.env("QT_QPA_PLATFORM", "wayland;xcb")
 hl.env("QT_WAYLAND_DISABLE_WINDOWDECORATION", "1")
 hl.env("QT_QPA_PLATFORMTHEME", "qt6ct")
+hl.env("QT_STYLE_OVERRIDE", "kvantum")
 
 -- GTK Variables
 
